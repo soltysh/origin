a2b952fe17793dd466d4f75d0af7b92d4084cfa1
Michal Minář <miminar@redhat.com>
2016-11-04 18:20:51 +0100

    Revert "UPSTREAM: docker/distribution: 1703: GCS: FileWriter.Size: include number of buffered bytes if the FileWriter is not closed"
    This reverts commit b6ba07ba132ec8a3f19f2bbbfd04647df4d82af0.

--- Godeps/_workspace/src/github.com/docker/distribution/registry/storage/driver/gcs/gcs.go
+++ Godeps/_workspace/src/github.com/docker/distribution/registry/storage/driver/gcs/gcs.go
@@ -493,9 +493,6 @@ func (w *writer) Write(p []byte) (int, error) {
 
 // Size returns the number of bytes written to this FileWriter.
 func (w *writer) Size() int64 {
-	if !w.closed {
-		return w.offset + int64(w.buffSize)
-	}
 	return w.size
 }
 
