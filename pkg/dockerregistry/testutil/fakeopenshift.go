a1ae5f8c4c5165c5f8c4df5493f49f20a7d4e970
Oleg Bulatov <obulatov@redhat.com>
2017-03-31 15:55:15 +0200

    Use docker image reference from ImageStream
    Fixes bug 1433232
    
    Signed-off-by: Oleg Bulatov <obulatov@redhat.com>

--- pkg/dockerregistry/testutil/fakeopenshift.go
+++ pkg/dockerregistry/testutil/fakeopenshift.go
@@ -73,6 +73,21 @@ func (fos *FakeOpenShift) GetImage(name string) (*imageapi.Image, error) {
 	return &image, nil
 }
 
+func (fos *FakeOpenShift) UpdateImage(image *imageapi.Image) (*imageapi.Image, error) {
+	fos.mu.Lock()
+	defer fos.mu.Unlock()
+
+	_, ok := fos.images[image.Name]
+	if !ok {
+		return nil, errors.NewNotFound(api.Resource("images"), image.Name)
+	}
+
+	fos.images[image.Name] = *image
+	fos.logger.Debugf("(*FakeOpenShift).images[%q] updated", image.Name)
+
+	return image, nil
+}
+
 func (fos *FakeOpenShift) CreateImageStream(namespace string, is *imageapi.ImageStream) (*imageapi.ImageStream, error) {
 	fos.mu.Lock()
 	defer fos.mu.Unlock()
@@ -133,7 +148,7 @@ func (fos *FakeOpenShift) CreateImageStreamMapping(namespace string, ism *imagea
 	}
 
 	_, err = fos.CreateImage(&ism.Image)
-	if err != nil {
+	if err != nil && !errors.IsAlreadyExists(err) {
 		return nil, err
 	}
 
@@ -300,6 +315,11 @@ func (fos *FakeOpenShift) imagesHandler(action core.Action) (bool, runtime.Objec
 			switch action := action.(type) {
 			case core.GetActionImpl:
 				image, err := fos.GetImage(action.Name)
+				return true, image, err
+			case core.UpdateActionImpl:
+				image, err := fos.UpdateImage(
+					action.Object.(*imageapi.Image),
+				)
 				return true, image, err
 			}
 			return fos.todo(action)
