9a3836aadfb8499f011cd835892f8efe1def35c4
Ravi Sankar Penta <rpenta@redhat.com>
2017-05-25 11:07:14 -0700

    Bug 1417641 - Make network diagnostic test pod image/protocol/port configurable
    This will be helpful in offline environments.

--- pkg/diagnostics/network/run_pod.go
+++ pkg/diagnostics/network/run_pod.go
@@ -32,6 +32,9 @@ type NetworkDiagnostic struct {
 	PreventModification bool
 	LogDir              string
 	PodImage            string
+	TestPodImage        string
+	TestPodProtocol     string
+	TestPodPort         int
 
 	pluginName    string
 	nodes         []kapi.Node
@@ -92,12 +95,6 @@ func (d *NetworkDiagnostic) Check() types.DiagnosticResult {
 		return d.res
 	}
 
-	if len(d.LogDir) == 0 {
-		d.LogDir = util.NetworkDiagDefaultLogDir
-	}
-	if len(d.PodImage) == 0 {
-		d.PodImage = util.NetworkDiagDefaultPodImage
-	}
 	d.runNetworkDiagnostic()
 	return d.res
 }
