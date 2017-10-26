5ce3d2c5d6e18221a79803b0a190620f4bb1aa06
Michal Fojtik <mfojtik@redhat.com>
2017-10-26 14:07:05 +0200

    fix serviceaccounts client

--- pkg/serviceaccounts/controllers/deleted_token_secrets_test.go
+++ pkg/serviceaccounts/controllers/deleted_token_secrets_test.go
@@ -11,10 +11,10 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	informers "k8s.io/client-go/informers"
+	externalfake "k8s.io/client-go/kubernetes/fake"
 	clientgotesting "k8s.io/client-go/testing"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/kubernetes/pkg/api"
-	"k8s.io/kubernetes/pkg/client/clientset_generated/internalclientset/fake"
 	"k8s.io/kubernetes/pkg/controller"
 )
 
@@ -180,7 +180,7 @@ func TestTokenDeletion(t *testing.T) {
 		// Re-seed to reset name generation
 		rand.Seed(1)
 
-		client := fake.NewSimpleClientset(tc.ClientObjects...)
+		client := externalfake.NewSimpleClientset(tc.ClientObjects...)
 		informerFactory := informers.NewSharedInformerFactory(client, controller.NoResyncPeriodFunc())
 		controller := NewDockercfgTokenDeletedController(
 			informerFactory.Core().V1().Secrets(),
