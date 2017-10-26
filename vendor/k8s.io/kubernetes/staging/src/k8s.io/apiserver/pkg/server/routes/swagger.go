bedd0ce1c85d7fc83fbff6bfbdc0908ea15a4e9c
Maciej Szulik <maszulik@redhat.com>
2017-10-26 17:27:10 +0200

    bump(k8s.io/kubernetes): c4e50365efea11ce5c75342bdae581a20cde8f0e

--- vendor/k8s.io/kubernetes/staging/src/k8s.io/apiserver/pkg/server/routes/swagger.go
+++ vendor/k8s.io/kubernetes/staging/src/k8s.io/apiserver/pkg/server/routes/swagger.go
@@ -32,5 +32,5 @@ type Swagger struct {
 // Install adds the SwaggerUI webservice to the given mux.
 func (s Swagger) Install(c *restful.Container) {
 	s.Config.WebServices = c.RegisteredWebServices()
-	swagger.RegisterSwaggerService(*s.Config, []*restful.Container{}, c)
+	swagger.RegisterSwaggerService(*s.Config, c)
 }
