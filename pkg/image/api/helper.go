23fea22e90e907d05501556c92d06b787d76866d
Michal Minář <miminar@redhat.com>
2017-11-22 17:15:19 +0100

    Version prefix matters when sorting tags names
    Make sure that 'v' prefix is taken into account.
    
    Sort identical tag names with 'v' prefix at the end.
    
    Signed-off-by: Michal Minář <miminar@redhat.com>

--- pkg/image/api/helper.go
+++ pkg/image/api/helper.go
@@ -983,6 +983,7 @@ type prioritizedTag struct {
 	tag      string
 	priority tagPriority
 	semver   semver.Version
+	prefix   string
 }
 
 func prioritizeTag(tag string) prioritizedTag {
@@ -993,7 +994,12 @@ func prioritizeTag(tag string) prioritizedTag {
 		}
 	}
 
-	short := strings.TrimLeft(tag, "v")
+	short := tag
+	prefix := ""
+	if strings.HasPrefix(tag, "v") {
+		prefix = "v"
+		short = tag[1:]
+	}
 
 	// 5.1.3
 	if v, err := semver.Parse(short); err == nil {
@@ -1001,6 +1007,7 @@ func prioritizeTag(tag string) prioritizedTag {
 			tag:      tag,
 			priority: tagPriorityFull,
 			semver:   v,
+			prefix:   prefix,
 		}
 	}
 
@@ -1011,6 +1018,7 @@ func prioritizeTag(tag string) prioritizedTag {
 				tag:      tag,
 				priority: tagPriorityMinor,
 				semver:   v,
+				prefix:   prefix,
 			}
 		}
 	}
@@ -1022,6 +1030,7 @@ func prioritizeTag(tag string) prioritizedTag {
 				tag:      tag,
 				priority: tagPriorityMinor,
 				semver:   v,
+				prefix:   prefix,
 			}
 		}
 	}
@@ -1030,6 +1039,7 @@ func prioritizeTag(tag string) prioritizedTag {
 	return prioritizedTag{
 		tag:      tag,
 		priority: tagPriorityOther,
+		prefix:   prefix,
 	}
 }
 
@@ -1047,7 +1057,10 @@ func (t prioritizedTags) Less(i, j int) bool {
 	}
 
 	cmp := t[i].semver.Compare(t[j].semver)
-	return cmp > 0 // the newer tag has a higher priority
+	if cmp > 0 { // the newer tag has a higher priority
+		return true
+	}
+	return cmp == 0 && t[i].prefix < t[j].prefix
 }
 
 // PrioritizeTags orders a set of image tags with a few conventions:
