soltysh/origin

commit 634f2c0dd2959f643d0bef089e0a667272d3b805

Don't call DeleteHostSubnetRules on a replayed Deleted event

pkg/network/node/subnets.go
@@ -59,6 +59,9 @@ func (node *OsdnNode) watchSubnets() {
 			node.AddHostSubnetRules(hs)
 			subnets[string(hs.UID)] = hs
 		case cache.Deleted:
+			if _, exists := subnets[string(hs.UID)]; !exists {
+				return
+			}
 			delete(subnets, string(hs.UID))
 			node.DeleteHostSubnetRules(hs)
 		}

commit fc5ecbc844d0cfb1fabfe96fa281dc5843cf6f4b

watchSubnets: Add missing return value
Fix the following build error:

    pkg/network/node/subnets.go:63:5: not enough arguments to return

This commit fixes an error introduced in
commit 634f2c0dd2959f643d0bef089e0a667272d3b805.

pkg/network/node/subnets.go
@@ -60,7 +60,7 @@ func (node *OsdnNode) watchSubnets() {
 			subnets[string(hs.UID)] = hs
 		case cache.Deleted:
 			if _, exists := subnets[string(hs.UID)]; !exists {
-				return
+				return nil
 			}
 			delete(subnets, string(hs.UID))
 			node.DeleteHostSubnetRules(hs)