d2a44cd78ce889ac8b47acba814f8070599f7b2c
Seth Jennings <sjenning@redhat.com>
2018-04-06 09:52:36 -0500

    UPSTREAM: Azure/go-autorest: 155: Retry with 429 status code

--- vendor/github.com/Azure/go-autorest/autorest/client.go
+++ vendor/github.com/Azure/go-autorest/autorest/client.go
@@ -24,6 +24,7 @@ const (
 
 var statusCodesForRetry = []int{
 	http.StatusRequestTimeout,      // 408
+	http.StatusTooManyRequests,     // 429
 	http.StatusInternalServerError, // 500
 	http.StatusBadGateway,          // 502
 	http.StatusServiceUnavailable,  // 503
