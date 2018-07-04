7e842ac8b379837fb43f88d97b9b26196727696f
Jan Wozniak <jwozniak@redhat.com>
2018-07-04 12:39:27 +0200

    bump(github.com/openshift/source-to-image): a49bab886b5d5c9eaeea698a6a058b0c8bf90076

--- vendor/github.com/openshift/source-to-image/pkg/build/strategies/sti/sti.go
+++ vendor/github.com/openshift/source-to-image/pkg/build/strategies/sti/sti.go
@@ -108,7 +108,7 @@ func New(client dockerpkg.Client, config *api.Config, fs fs.FileSystem, override
 		config.PullAuthentication,
 		fs,
 	)
-	tarHandler := tar.NewParanoid(fs)
+	tarHandler := tar.New(fs)
 	tarHandler.SetExclusionPattern(excludePattern)
 
 	builder := &STI{
