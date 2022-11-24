2ea96e770598150e89cdfdbe8a1aa59278c424cb
Maciej Szulik <maszulik@redhat.com>
2022-11-24 16:38:21 +0100

    origin-e2e.test

---
+++ cmd/e2e/origin_e2e_test.go
@@ -0,0 +1,54 @@
+package e2e
+
+// This is a duplicate of github.com/openshift/kubernets/openshift-hack/e2e/kube_e2e_test.go
+
+import (
+	"flag"
+	"fmt"
+	"math/rand"
+	"os"
+	"testing"
+	"time"
+
+	// Never, ever remove the line with "/ginkgo". Without it,
+	// the ginkgo test runner will not detect that this
+	// directory contains a Ginkgo test suite.
+	// See https://github.com/kubernetes/kubernetes/issues/74827
+	// "github.com/onsi/ginkgo/v2"
+
+	"k8s.io/component-base/version"
+	"k8s.io/kubernetes/test/e2e"
+	"k8s.io/kubernetes/test/e2e/framework"
+	"k8s.io/kubernetes/test/e2e/framework/testfiles"
+)
+
+func TestMain(m *testing.M) {
+	var versionFlag bool
+	flag.CommandLine.BoolVar(&versionFlag, "version", false, "Displays version information.")
+
+	// Register test flags, then parse flags.
+	e2e.HandleFlags()
+
+	if versionFlag {
+		fmt.Printf("%s\n", version.Get())
+		os.Exit(0)
+	}
+
+	framework.AfterReadingAllFlags(&framework.TestContext)
+
+	// TODO: Deprecating repo-root over time... instead just use gobindata_util.go , see #23987.
+	// Right now it is still needed, for example by
+	// test/e2e/framework/ingress/ingress_utils.go
+	// for providing the optional secret.yaml file and by
+	// test/e2e/framework/util.go for cluster/log-dump.
+	if framework.TestContext.RepoRoot != "" {
+		testfiles.AddFileSource(testfiles.RootFileSource{Root: framework.TestContext.RepoRoot})
+	}
+
+	rand.Seed(time.Now().UnixNano())
+	os.Exit(m.Run())
+}
+
+func TestE2E(t *testing.T) {
+	e2e.RunE2ETests(t)
+}
