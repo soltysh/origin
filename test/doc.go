457f5d6603b40ab663e8fa28cbf6dc64fca585af
Maciej Szulik <maszulik@redhat.com>
2023-02-16 12:53:06 +0100

    Add ginkgo/v2 magnet

--- test/doc.go
+++ test/doc.go
@@ -1,11 +1,12 @@
 //go:build tools
 // +build tools
 
-// Package test contains cross-functional test suites for OpenShift 3.
+// Package test contains cross-functional test suites for OpenShift.
 package test
 
 import (
 	_ "github.com/go-bindata/go-bindata/go-bindata"
+	_ "github.com/onsi/ginkgo/v2/ginkgo"
 	_ "github.com/openshift/build-machinery-go"
 	_ "github.com/stretchr/testify/require"
 	_ "k8s.io/kubernetes/pkg/registry/registrytest"
